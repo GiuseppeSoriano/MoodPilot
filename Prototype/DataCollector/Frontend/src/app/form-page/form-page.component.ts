d876b5488373e0e07639a8b8f07c9d87a99ee7b6
Giuseppe Soriano <102223105+GiuseppeSoriano@users.noreply.github.com>
2025-01-12 13:07:14 +0100

    feat(Frontend): Loading page added

--- Prototype/DataCollector/Frontend/src/app/form-page/form-page.component.ts
+++ Prototype/DataCollector/Frontend/src/app/form-page/form-page.component.ts
@@ -9,11 +9,12 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 import { DragDropComponent } from "../utility/drag-drop/drag-drop.component";
 import { ReportService } from '../services/report/report.service';
+import { LoadingComponent } from "../utility/loading/loading.component";
 
 @Component({
   selector: 'app-form-page',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatExpansionModule, MatFormFieldModule, MatInputModule, MatIcon, MatRadioModule, MatButtonModule, DragDropComponent],
+  imports: [CommonModule, ReactiveFormsModule, MatExpansionModule, MatFormFieldModule, MatInputModule, MatIcon, MatRadioModule, MatButtonModule, DragDropComponent, LoadingComponent],
   templateUrl: './form-page.component.html',
   styleUrl: './form-page.component.css'
 })
@@ -21,6 +22,8 @@ export class FormPageComponent {
   rideFeedbackForm: FormGroup;
   ratings = [1, 2, 3, 4, 5];
   stars = [1, 2, 3, 4, 5];
+
+  loading = false; // Per mostrare il loading spinner
 
   videoFile: File | Blob | null = null; // Per salvare il video caricato
 
@@ -76,13 +79,17 @@ export class FormPageComponent {
 
     console.log('Invio del report:', formData);
 
+    this.loading = true; // Mostra il loading spinner
+
     // Invia la richiesta tramite il service
     this.reportService.sendReport(formData).subscribe({
       next: (response) => {
+        this.loading = false; // Nasconde il loading spinner
         console.log('Report inviato con successo:', response);
         alert('Report inviato con successo!');
       },
       error: (error) => {
+        this.loading = false; // Nasconde il loading spinner
         console.error('Errore nell\'invio del report:', error);
         alert('Errore durante l\'invio del report.');
       },
